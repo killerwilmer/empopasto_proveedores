c40dfa6b39a74701d531909c59f3e8c3a5c212e4
Wilmer Arteaga <killerwilmer@gmail.com>
2012-10-31 10:41:39 -0500

    Haciendo pruebas con ScaffoldController.

---
+++ default/app/controllers/actividad_controller.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of actividad_controller
+ *
+ * @author wilmerarteaga
+ */
+class ActividadController extends ScaffoldController {
+    public $model = 'actividad';
+}
+
+?>
