c1210a05c8ea3f4d0ca5e309c26f95cd36968609
Wilmer Arteaga <killerwilmer@gmail.com>
2012-10-31 12:49:20 -0500

    Creando el controller para el proveedor.

---
+++ default/app/controllers/proveedores_controller.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of proveedores_controller
+ *
+ * @author wilmerarteaga
+ */
+class ProveedoresController extends ApplicationController {
+    public function index(){
+        
+    }
+    
+    public function nuevo(){
+        
+    }
+}
+
+?>
