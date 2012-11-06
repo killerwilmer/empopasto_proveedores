cfa725db83b031427ce4d4b8344957a47ae61284
Wilmer Arteaga <killerwilmer@gmail.com>
2012-11-06 15:46:34 -0500

    save proveedores.

--- default/app/controllers/registro_controller.php
+++ default/app/controllers/registro_controller.php
@@ -10,12 +10,24 @@
  *
  * @author wilmerarteaga
  */
-Load::models('proveedores');
-
 class RegistroController extends ApplicationController {
-        public function nuevonormal() {
-        View::template('index/index');        
+
+    public function nuevonormal() {
+        Load::model('proveedores');
+        View::template('index/index');
+
+        if (Input::hasPost('proveedores')) {
+
+            $miProveedor = new Proveedores(Input::post('proveedores'));
+            if ($miProveedor->guardar(Input::post('proveedores'))) {
+
+                Flash::valid('Campos guardados Exitosamente...!!!');
+            } else {
+                Flash::warning('No se Pudieron Guardar los Datos...!!!');
+            }
+        }
     }
+
 }
 
 ?>
--- default/app/models/proveedores.php
+++ default/app/models/proveedores.php
@@ -12,6 +12,16 @@
  */
 class Proveedores extends ActiveRecord {
 
+    public function initialize() {
+
+        $this->validates_presence_of('email', 'message: Debe escribir un <b>correo electronico</b>');
+        $this->validates_email_in('email', 'message: Debe escribir un <b>correo electronico</b> válido');
+        $this->validates_presence_of('tipo_proveedor_id', 'message: Debe escojer un <b>Tipo de proveedor.</b>');
+        $this->validates_presence_of('tipo_entidad_id', 'message: Debe escojer un <b>Tipo de entidad.</b>');
+        $this->validates_presence_of('ciudad_id', 'message: Debe escojer una <b>Ciudad.</b>');
+        $this->validates_presence_of('tipo_identificacion_id', 'message: Debe escojer un  <b>Tipo de identificación.</b>');
+    }
+
     /**
      * Devuelve los usuarios de la bd Paginados.
      * 
@@ -20,6 +30,18 @@ class Proveedores extends ActiveRecord {
      */
     public function paginar($pagina = 1) {
         return $this->paginate("page: $pagina");
+    }
+
+    public function guardar($proveedores) {
+
+        $this->begin(); //iniciamos la trasaccion
+
+        if (!$this->save($proveedores)) {
+            $this->rollback();
+            return FALSE;
+        }
+        $this->commit();
+        return TRUE;
     }
 
 }
